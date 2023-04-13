4c8685ad88a88dafed55cd89bd5835c57b28a24a
DoubleJCafe <jansejos@oregonstate.edu>
2023-04-13 00:48:55 -0700

    Test 3

--- KthElement.py
+++ KthElement.py
@@ -20,26 +20,23 @@ def merg(arr1, arr2):
     sortedArray = []
     arr1Index = 0
     arr2Index = 0
-    start = 0
     while arr1Index < len(arr1) and arr2Index < len(arr2):
         if arr1[arr1Index] < arr2[arr2Index]:
-            sortedArray[start] = arr1[arr1Index]
+            sortedArray.append(arr1[arr1Index])
             arr1Index = arr1Index + 1
         else:
-            sortedArray[start] = arr2[arr2Index]
+            sortedArray.append(arr2[arr2Index])
             arr2Index = arr2Index + 1
-        start = start + 1
 
     while arr1Index < len(arr1):
-        sortedArray[start] = arr1[arr1Index]
+        sortedArray.append(arr1[arr1Index])
         arr1Index = arr1Index + 1
-        start = start + 1
 
     while arr2Index < len(arr2):
-        sortedArray[start] = arr2[arr2Index]
+        sortedArray.append(arr2[arr2Index])
         arr2Index = arr2Index + 1
-        start = start + 1
     return sortedArray
+
 
 
 
